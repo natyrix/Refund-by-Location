e6f6619edcf0afb36ca59ff396f3f1942fdcf719
Natnael Melese <natrix277@gmail.com>
2022-10-27 21:47:13 -0400

    README updated

--- betting_example/migrations/2_deploy_contracts.js
+++ betting_example/migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-var Casino = artifacts.require("./Casino.sol");
+var Casino = artifacts.require("Casino.sol");
 module.exports = function(deployer) {
-  deployer.deploy(web3.toWei(0.1, 'ether'), 100, {gas: 3000000});
+  deployer.deploy(Casino);
 };
